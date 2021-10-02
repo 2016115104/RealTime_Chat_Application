dda3696848266b4656aaf8ed497b0aef852289df
Swathy V <swathyvivekanandan@gmail.com>
2021-10-01 18:01:24 -0700

    socket

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const socketio = require("socket.io")
 const app = express()
 const port = 3000
 app.use(express.static(path.join(__dirname,'public')))
-
+const server = http.createServer(app)
+const io = socketio(server)
+io.on('connection',socket => {
+  console.log("connected")
+})
 
 
 
