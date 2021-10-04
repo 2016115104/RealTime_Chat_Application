07e8b008a991227a138633f25aa45a2e75315d9b
sow04 <sowman04@gmail.com>
2021-10-03 20:56:34 -0700

    comments

--- index.js
+++ index.js
@@ -2,22 +2,39 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const socketio = require("socket.io")
+
 const app = express()
 const port = 3000
+
 app.use(express.static(path.join(__dirname,'public')))
+
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 const io = socketio(server)
+
+ // connection
 io.on('connection',socket => {
+ 
   console.log("connected")
+
+  // server to client msg emit - msg => name of the message to be send
   socket.emit('msg','Welcome!')
+
+  
   socket.broadcast.emit('msg','User joined the chat');
+
+
   socket.on('disconnect',()=>{
     io.emit('msg','User left the chat')
   })
+
+
+  // getting the msg from client
   socket.on('chat-msg',sentMsg =>{
-    console.log(sentMsg)
+
+    // send msg to client
+    io.emit('msg',sentMsg);
   })
 
 })
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,16 @@
 const socket = io();
 const form = document.getElementById('chat-form')
+
+// getting the msg from server
 socket.on('msg',msg => {
     console.log(msg)
 })
+
+
+
 form.addEventListener('submit',e => {
     e.preventDefault();
     const sentMsg = e.target.elements.msg.value
+    // emitting typed msg from client to server
     socket.emit('chat-msg',sentMsg)
 } )
